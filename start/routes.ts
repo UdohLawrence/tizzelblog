4da8b75afce939fcb9773ff5edb8395bc847ab6d
UdohLawrence <lawrenceaudoh@gmail.com>
2025-03-13 18:47:36 +0100

    create routes for the register controller

--- start/routes.ts
+++ start/routes.ts
@@ -1,3 +1,7 @@
+const RegisterController = () => import('#controllers/register_controller')
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/register', [RegisterController, 'show'])
+router.post('/register', [RegisterController, 'store'])
