0c633d7b7e4b36dcb08b7cb28374691309a240e7
UdohLawrence <lawrenceaudoh@gmail.com>
2025-03-13 22:21:49 +0100

    FEATURE: add ability to successfully create a user

--- app/controllers/register_controller.ts
+++ app/controllers/register_controller.ts
@@ -1,10 +1,26 @@
+import User from '#models/user'
+import { registerValidator } from '#validators/register'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class RegisterController {
   async show({ view }: HttpContext) {
     return view.render('pages/auth/register')
   }
-  async store() {
-    //
+  async store({ auth, request, response, session }: HttpContext) {
+    // const data = request.only(['first_name', 'last_name', 'username', 'email', 'password'])
+    const payload = await request.validateUsing(registerValidator)
+
+    const user = await User.create(payload)
+
+    await auth.use('web').login(user)
+
+    session.flash({
+      notification: {
+        type: 'success',
+        message: 'Successfully registered!',
+      },
+    })
+
+    return response.redirect('/')
   }
 }
