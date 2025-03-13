6dd618cf329c5c4c6e5c0d122154dcd2f2222d57
UdohLawrence <lawrenceaudoh@gmail.com>
2025-03-13 18:46:14 +0100

    create a controller for the rregister routes

---
+++ app/controllers/register_controller.ts
@@ -0,0 +1,10 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class RegisterController {
+  async show({ view }: HttpContext) {
+    return view.render('pages/auth/register')
+  }
+  async store() {
+    //
+  }
+}
