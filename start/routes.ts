b99fc1b99a9d3a7a7b925da6b15a6e1a686ced57
UdohLawrence <lawrenceaudoh@gmail.com>
2025-03-15 22:20:36 +0100

    CODE: implement login functionality

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,10 @@
 const RegisterController = () => import('#controllers/register_controller')
+const LoginController = () => import('#controllers/login_controller')
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
 
 router.get('/register', [RegisterController, 'show'])
 router.post('/register', [RegisterController, 'store'])
+router.get('/login', [LoginController, 'show'])
+router.post('/login', [LoginController, 'store'])
