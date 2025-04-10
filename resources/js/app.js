d8bde3c5957dfd10cc51116578aa3fbc237747ce
UdohLawrence <lawrenceaudoh@gmail.com>
2025-04-10 18:31:10 +0100

    create the javascript code for mobile responsiveness

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+const hamburger = document.getElementById('hamburger')
+const mobileNav = document.getElementById('mobile-nav')
+
+const toggleMobileMenu = () => {
+
+  if (mobileNav.classList.contains('hidden')) {
+    hamburger.innerHTML = '&times;'
+    mobileNav.classList.remove('hidden')
+  } else {
+    hamburger.innerHTML = '&#9776;'
+    mobileNav.classList.add('hidden')
+  }
+}
