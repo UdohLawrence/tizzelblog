05eb58e82138b3268db621a82fb7aa84005c665f
UdohLawrence <lawrenceaudoh@gmail.com>
2025-04-11 17:17:54 +0100

    implemented logout functionality

--- app/controllers/auth/logout_controller.ts
+++ app/controllers/auth/logout_controller.ts
@@ -1,5 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class LogoutController {
-  async handle({}: HttpContext) {}
+  async handle({ auth, response }: HttpContext) {
+    await auth.use('web').logout()
+
+    return response.redirect('/')
+  }
 }
