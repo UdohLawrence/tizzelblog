18833a4a19f666b9ecf2f77a34963743167d4f1a
UdohLawrence <lawrenceaudoh@gmail.com>
2025-04-11 15:39:39 +0100

    CREATE: aliases for the auth routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,16 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
+const RegisterController = () => import('#controllers/auth/register_controller')
+const LoginController = () => import('#controllers/auth/login_controller')
+const LogoutController = () => import('#controllers/auth/logout_controller')
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router
+  .group(() => {
+    router.get('/register', [RegisterController, 'show']).as('register.show')
+    router.post('/register', [RegisterController, 'store']).as('register.store')
+    router.get('/login', [LoginController, 'show']).as('login.show')
+    router.post('/login', [LoginController, 'store']).as('login.store')
+    router.post('/logout', [LogoutController, 'store']).as('logout')
+  })
+  .as('auth')
